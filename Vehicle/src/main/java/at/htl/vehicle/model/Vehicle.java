fc9ec6a43f3ed87949b5981295530bd9cfa3c8c1
Jonas Schürz <jonas.schuerz@gmx.at>
2018-10-16 09:27:56 +0200

    09.10.2018 - Vehicle - K05

---
+++ Vehicle/src/main/java/at/htl/vehicle/model/Vehicle.java
@@ -0,0 +1,47 @@
+package at.htl.vehicle.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Vehicle {
+
+    private Long id;
+    private String brand;
+    private String type;
+
+    public Vehicle() {
+
+    }
+
+    public Vehicle(String brand, String type) {
+        this.brand = brand;
+        this.type = type;
+    }
+
+    //region Getter and Setter
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    //endregion
+
+}
